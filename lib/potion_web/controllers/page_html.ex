b8be8ed70d2ce42c94bc3e3ee877baf95faac89e
Pedro Tolvo <tolvo.pedro@gmail.com>
2025-01-30 22:15:18 -0300

    strt

---
+++ lib/potion_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule PotionWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use PotionWeb, :html
+
+  embed_templates "page_html/*"
+end
---
+++ test/potion_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule PotionWeb.ErrorJSONTest do
+  use PotionWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert PotionWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert PotionWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
